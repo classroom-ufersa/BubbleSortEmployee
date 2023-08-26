dd51155426a2a7db1bbdbefe2f2afb837960829c
PedroElric <138056745+pedroelr1c@users.noreply.github.com>
2023-08-26 17:31:22 +0000

    codigos main

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#include <time.h>
 #include "funcionario.c"
 
 int main() {
+
     int vetor[] = {5, 3, 2, 4, 7, 1, 0, 6};
     int tamanho = sizeof(vetor) / sizeof(int);
     int i;
@@ -11,5 +13,12 @@ int main() {
         printf("%d ", vetor[i]);
     }
     printf(" Ordenado com Bubble Sort ");
+
+    clock_t inicio = clock();
+    // Executar o algoritmo
+    double tempo = (double)(clock() - inicio) / CLOCKS_PER_SEC;
+    tempo = tempo * 1000; //milisegundos
+    printf("Tempo de execucao: %.50f\n", tempo);
+
     return 0;
 }
