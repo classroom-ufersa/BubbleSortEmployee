aaad4a666a815d4e47ebc5b4a146af6d88f34a30
Johan Queiroz <123979951+johanpq@users.noreply.github.com>
2023-08-26 18:21:57 +0000

    adicionado função BubbleSort

--- funcionario.c
+++ funcionario.c
@@ -5,3 +5,16 @@ struct Funcionario {
     char cargo[50];
     int documento;
 };
+
+void bubbleSort(int *vetor, int tamanho) {
+    int i, j, aux;
+    for (i = 0; i < tamanho; i++) {
+        for (j = 0; j < tamanho - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+            }
+        }
+    }
+}
